a4010e4153033a4aff9dddb51ba1acdac29012c2
Hugo_古学冬 <hokdung@outlook.com>
2025-03-15 21:05:11 +0800

    feat: add env support

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,38 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
-export const onRequestGet = async ({request}) => {
-	 const { method, headers, url } = request 
-	 const searchParams = new URL(url).searchParams
-	 if (searchParams.has('dns')) {
-	 return await fetch(doh + '?dns=' + searchParams.get('dns'), {
+export const onRequestGet = async ({ request, env }) => {
+    const { method, headers, url } = request
+    const searchParams = new URL(url).searchParams
+    const doh = env.DOH_ADDRESS || 'https://security.cloudflare-dns.com/dns-query';
+    const dohjson = env.DOH_JSON_ADDRESS || 'https://security.cloudflare-dns.com/dns-query';
+
+    if (searchParams.has('dns')) {
+        return await fetch(doh + '?dns=' + searchParams.get('dns'), {
             method: 'GET',
             headers: {
                 'Accept': contype,
             }
         });
-	 } else if (method== 'GET' && headers.get('Accept')==jstontype) {
+    } else if (method == 'GET' && headers.get('Accept') == jstontype) {
         const search = new URL(url).search
-         return await fetch(dohjson + search, {
+        return await fetch(dohjson + search, {
             method: 'GET',
             headers: {
                 'Accept': jstontype,
             }
         });
     } else {
-        return new Response("", {status: 404})
+        return new Response("", { status: 404 })
     }
 
 }
 
-export const onRequestPost = async ({ request }) => {
-	const { headers } = request 
-  if (headers.get('content-type')==contype) {
+export const onRequestPost = async ({ request, env }) => {
+    const { headers } = request
+    const doh = env.DOH_ADDRESS || 'https://security.cloudflare-dns.com/dns-query';
+
+    if (headers.get('content-type') == contype) {
         return fetch(doh, {
             method: 'POST',
             headers: {
@@ -38,7 +41,7 @@ export const onRequestPost = async ({ request }) => {
             },
             body: request.body,
         });
-		 } else {
-        return new Response("", {status: 404})
+    } else {
+        return new Response("", { status: 404 })
     }
 }
